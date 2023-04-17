3fc417a49956782e479b899244409a7ef198c4f1
NezhaOUBELLA <oubella.nezha2002@gmail.com>
2023-04-17 01:13:56 +0000

    First Commit (Create the project)

---
+++ ControleJEE-SpringBoot/src/test/java/ma/enset/ControleJEESpringBoot/ControleJeeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.ControleJEESpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleJeeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
